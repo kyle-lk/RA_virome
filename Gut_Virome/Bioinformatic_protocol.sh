9944a23d3989426a337e229c765ed194fdbf2aa4
kyle <380548022@qq.com>
2022-10-26 01:47:57 +0800

    Update Bioinformatic_protocol.sh

--- Gut_Virome/Bioinformatic_protocol.sh
+++ Gut_Virome/Bioinformatic_protocol.sh
@@ -1,1 +1,49 @@
+#!bin/bash
+
+
+###  1. Quality control
+#############################
+
+
+
+
+
+
+#### 2. de novo assembly 
+##############################
+
+
+
+
+#### 3. non-redundant contigs set 
+##############################
+
+
+#### 4. Candidate viral contigs selection
+##############################
+
+
+
+
+#### 5. CheckV analysis
+##############################
+
+
+#### 6. lytic vs lysogenic phage identification
+##############################
+
+
+
+
+#### 7. AMGs annotation
+##############################
+
+
+
+
+
+
+
+
+
 
